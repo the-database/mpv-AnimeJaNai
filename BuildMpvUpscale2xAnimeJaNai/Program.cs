496732f29f611be27033c119cf4bc652a67d42c5
the-database <25811902+the-database@users.noreply.github.com>
2026-06-16 12:17:14 -0400

    manager 0.3.2

--- BuildMpvUpscale2xAnimeJaNai/Program.cs
+++ BuildMpvUpscale2xAnimeJaNai/Program.cs
@@ -24,7 +24,7 @@ const string VsMlrtCudaVersion    = "v16.test1";
 const string AjiVersion           = "v0.4.0";       // github.com/the-database/animejanai-inference release tag (multi-GPU kernels + P010; CUDA graphs ON)
 const string SevenZipVersion      = "2501";         // 7-zip "extra" standalone console version
 const string MpvNetVersion        = "v7.1.2.0";
-const string ManagerVersion       = "0.3.1";        // github.com/the-database/AnimeJaNaiManager release tag (AnimeJaNai Manager)
+const string ManagerVersion       = "0.3.2";        // github.com/the-database/AnimeJaNaiManager release tag (AnimeJaNai Manager)
 
 // DirectML backend runtime (backend=DirectML in animejanai.conf). These are
 // the last DirectML-flavored releases: Microsoft moved DML to sustained
